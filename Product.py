81c023afd2f7b542890cdd1f8309b62168c1c847
Regina Beretta <reginabmre@gmail.com>
2020-12-07 12:04:25 -0400

    D from CRUD implemented

--- Product.py
+++ Product.py
@@ -1,7 +1,6 @@
 class Product:
-
     __name: str
-    __dscription: str
+    __description: str
     __price: float
 
     def __init__(self, name, description, price):
@@ -33,13 +32,12 @@ class Product:
         print("Price: " + str(self.__price))
 
 
-
 def show_menu() -> int:
     option = -1
 
-    while(option < 0 or option > 4):
+    while option < 0 or option > 4:
         print()
-        print(40*'*')
+        print(40 * '*')
         option = int(input("""What do you want to do?
                        1 - Add a new product
                        2 - Search for a product
@@ -48,6 +46,7 @@ def show_menu() -> int:
                        0 - Finish 
                        """))
     return option
+
 
 def add_new_product() -> Product:
     print("Let's add a new product!")
@@ -61,17 +60,20 @@ def add_new_product() -> Product:
 
 def search_product(products_list, key) -> Product:
     for p in products_list:
-        if (p.get_name() == key):
+        if p.get_name() == key:
             return p
 
     print("There is no product named " + key)
     return None
 
-def  update_product(products_list):
+
+def update_product(products_list):
     key = input("What is the name of the product you want to update? ")
     p = search_product(products_list, key)
 
-    if (p != None):
+    if p is None:
+        print("Product not found!")
+    else:
         print("Let's update it then!")
         print("Current name: " + p.get_name())
         name = input("New name: ")
@@ -85,29 +87,35 @@ def  update_product(products_list):
         price = input("New price: ")
         p.set_price(price)
         print("Product successfully updated!")
-    else :
-        print("Product not found!")
+
 
 def delete_product(products_list):
+    key = input("What is the name of the product you want to delete? ")
+    p = search_product(products_list, key)
 
+    if p is None:
+        print("Product not found!")
+
+    else:
+        products_list.remove(p)
 
 
 option = -1
 products_list = list()
 
-while(option != 0):
+while option != 0:
     option = show_menu()
 
-    if(option == 1):
+    if option == 1:
         products_list.append(add_new_product())
-    elif(option == 2):
+    elif option == 2:
         key = input("What is the name of the product you want to search for? ")
         p = search_product(products_list, key)
-        if(p != None):
+        if p is not None:
             p.print()
-    elif(option == 3):
+    elif option == 3:
         update_product(products_list)
-    elif(option == 4):
+    elif option == 4:
         delete_product(products_list)
-    elif (option > 4 or option < 0):
+    elif option > 4 or option < 0:
         print("Invalid option! Try again!")
